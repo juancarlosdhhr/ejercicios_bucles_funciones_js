dbc9539a1fab6c4d3749feceb045a641b633e5db
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-07-01 13:09:46 +0100

    8

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,2 +1,40 @@
+/* Desarrolla una función que reciba un país por parámetro y devuelva su capital.
 
+Utiliza un objeto para almacenar los países y sus capitales.
+
+La función debe manejar casos en los que el país no esté en la lista, devolviendo un mensaje adecuado. */
+
+const capitals = {
+    Spain: 'Madrid',
+    France: 'Paris',
+    Italy: 'Rome',
+    Germany: 'Berlin',
+    Portugal: 'Lisbon',
+    Poland: 'Warsaw',
+    Greece: 'Athens',
+    Austria: 'Vienna',
+    Hungary: 'Budapest',
+    Ireland: 'Dublin'
+  };
+  
+  function getCapital(country) {
     
+for (const key in capitals) {
+
+if (key === country) {
+return capitals [key];
+
+  }
+else {
+return `The capital of ${country} is not in the list`;
+
+}
+}
+
+}
+
+
+console.log (getCapital('Spain'));
+console.log (getCapital('Croatia'));
+  
+  
