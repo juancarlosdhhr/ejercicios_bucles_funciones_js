6aeb5b7c8fd824c0e6ec0cd226e28cdf74530524
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-07-02 01:07:55 +0100

    6

---
+++ ejercicio11.js
@@ -0,0 +1,42 @@
+ /* Escribe una función que calcule el promedio de duración de una lista de películas.
+
+Recibe un array de películas, cada una con su duración en minutos.
+
+La función debe devolver el promedio de duración de todas las películas.
+ */
+
+const movies = [
+    { title: 'Inception', duration: 148 },
+    { title: 'The Dark Knight', duration: 152 },
+    { title: 'Interstellar', duration: 169 },
+    { title: 'Dunkirk', duration: 106 },
+    { title: 'The Prestige', duration: 130 },
+    { title: 'Memento', duration: 113 },
+    { title: 'Batman Begins', duration: 140 },
+    { title: 'The Dark Knight Rises', duration: 164 },
+    { title: 'Tenet', duration: 150 },
+    { title: 'Insomnia', duration: 118 }
+  ];
+  
+  function averageMovieDuration(movies) {
+    let TotalDuration = 0;
+
+    for (let i = 0; i < movies.length; i++) {
+    TotalDuration += movies[i].duration;
+
+}
+
+    //Ahora hay que calcular el promedio
+
+    const AverageDuration = TotalDuration / movies.length;
+
+    return AverageDuration;
+
+   
+
+
+  }
+
+  console.log (averageMovieDuration(movies));
+  
+  
---
+++ ejercicio12.js
@@ -0,0 +1,36 @@
+/* Desarrolla una función que busque en un array de objetos representando mutantes si existe alguno 
+con un poder específico y retorne un mensaje indicando si fue encontrado o no.
+
+Considera el caso de múltiples mutantes con el mismo poder.
+
+ */
+
+const mutants = [
+    { name: 'Wolverine', power: 'regeneration' },
+    { name: 'Magneto', power: 'magnetism' },
+    { name: 'Professor X', power: 'telepathy' },
+    { name: 'Jean Grey', power: 'telekinesis' },
+    { name: 'Rogue', power: 'power absorption' },
+    { name: 'Storm', power: 'weather manipulation' },
+    { name: 'Mystique', power: 'shape-shifting' },
+    { name: 'Beast', power: 'superhuman strength' },
+    { name: 'Colossus', power: 'steel skin' },
+    { name: 'Nightcrawler', power: 'teleportation' }
+  ];
+  
+  function findMutantByPower(mutants, power) {
+   for (let i=0; i < mutants.length; i++) {
+
+    if (mutants [i].power === power){
+ return `The mutant found: ${mutants[i].name}  , has the power to ${power}`;
+
+    }
+   }
+
+   return `Mo mutant with the power ${power} has been found`;
+  }
+
+  console.log(findMutantByPower(mutants, 'telepathy'));
+  console.log(findMutantByPower(mutants, 'premonition'));
+  
+  
